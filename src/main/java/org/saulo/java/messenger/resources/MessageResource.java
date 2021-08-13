3d7a4c558133aa1ef405d3781b1af07789d13be5
Saulo Mendes <saulomendes_br@hotmail.com>
2021-08-12 22:10:33 -0300

    PUT and DELETE method

--- src/main/java/org/saulo/java/messenger/resources/MessageResource.java
+++ src/main/java/org/saulo/java/messenger/resources/MessageResource.java
@@ -3,8 +3,10 @@ package org.saulo.java.messenger.resources;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -37,5 +39,21 @@ public class MessageResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Message getMessageId(@PathParam("messageId") long messageId) {
 		return messageService.getMessage(messageId);
+	}
+	
+	@PUT
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Message updateMessage(@PathParam("messageId") long messageId, Message message) {
+		message.setId(messageId);
+		return messageService.updateMessage(message);
+	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void deleteMessage(@PathParam("messageId") long messageId) {
+		messageService.removeMessage(messageId);
 	}
 }
